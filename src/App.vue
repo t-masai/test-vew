2cc8f3760aa117ac6a872dc092d52cd41bf25be8
t-masai <tmasai47@gmail.com>
2019-01-22 22:15:20 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Home v-if="!isLogin">
+</Home>
+    <Editor V-if="isLogin :user="userData"">
+</Editor>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import Editor from "./components/Editor.vue";
+
+export default {
+  name: 'app',
+  data () {
+    return {
+       isLogin: false
+    };
+  },
+
+created: function() {
+firebase. auth(). onAuthStateChanged( user => { console. log( user);
+console. log( user); 
+if (user) { 
+this. isLogin = true; 
+this.userData = user;
+} else {
+this. isLogin = false;
+this.userData = null;
+};
+});
+},
+
+components: {
+Home: Home,
+Editor: Editor
+  }
+};
+</script>
